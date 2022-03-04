3c698ba24c359465b62254185b754a159cdfc3bd
Giovanni <giomadalozzo@gmail.com>
2022-03-04 14:41:34 -0300

    Initial commit

---
+++ moviedb-mvvm/moviedb-mvvm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  moviedb-mvvm
+//
+//  Created by Giovanni Madalozzo on 04/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ moviedb-mvvm/moviedb-mvvmUITests/moviedb_mvvmUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  moviedb_mvvmUITestsLaunchTests.swift
+//  moviedb-mvvmUITests
+//
+//  Created by Giovanni Madalozzo on 04/03/22.
+//
+
+import XCTest
+
+class moviedb_mvvmUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
