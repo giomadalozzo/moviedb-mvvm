3a331c1bab958fa2cdbbd6fb38282a708d7fa217
Giovanni <giomadalozzo@gmail.com>
2022-03-09 14:05:15 -0300

    Added genres API on app startup

--- moviedb-mvvm/moviedb-mvvm/AppDelegate.swift
+++ moviedb-mvvm/moviedb-mvvm/AppDelegate.swift
@@ -2,4 +2,18 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        MovieAPI().requestGenres{ _ in }
+        return true
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        MovieAPI().localGenresArray = []
+        MovieAPI().requestGenres{ _ in }
+        }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        MovieAPI().localGenresArray = []
+        MovieAPI().requestGenres{ _ in }
+        }
 }
--- moviedb-mvvm/moviedb-mvvm/ViewModel/API/MovieAPI.swift
+++ moviedb-mvvm/moviedb-mvvm/ViewModel/API/MovieAPI.swift
@@ -1,9 +1,37 @@
 import UIKit
 
 class MovieAPI{
+    var localGenresArray: [Genres] = []
+    
+    func requestGenres(completionHandler: @escaping ([Genres]) -> Void) {
+        let urlString = "https://api.themoviedb.org/3/genre/movie/list?api_key=1b312813cf6df1bf51d1ada49057b17d&language=en-US"
+        let url = URL(string: urlString)!
+    
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data,
+                  let json = try? JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed),
+                  let dictionary = json as? [String: Any],
+                  let genres = dictionary["genres"] as? [[String: Any]]
+            else {
+                completionHandler([])
+                return
+            }
+            
+            for genreDictionary in genres {
+                guard let id = genreDictionary["id"] as? Int,
+                      let genre = genreDictionary["name"] as? String
+                else { continue }
+                
+                let genres = Genres(id: id, genre: genre)
+                self.localGenresArray.append(genres)
+            }
+            
+            completionHandler(self.localGenresArray)
+        }
+        .resume()
+    }
     
     func requestPopularMovies(completionHandler: @escaping ([Film]) -> Void) {
-        
         let urlString = "https://api.themoviedb.org/3/movie/now_playing?api_key=1b312813cf6df1bf51d1ada49057b17d&language=en-US&page=1"
         let url = URL(string: urlString)!
         
